c4fb5d195d33c51c5bb9f472dbaffc77a486ddf3
Imane316 <imane.ourraoui175@gmail.com>
2023-06-09 17:47:20 +0200

    up

--- routes.js
+++ routes.js
@@ -5,24 +5,23 @@ let categoryController = require('./controllers/categoryController');
 const jwt = require('jsonwebtoken');
 const jwtKey = 'my_secret_key';
 const jwtExpirySeconds = 200;
+let router = express.Router();
 
 function isAuthorized (req, res, next) {
     if (typeof req.headers.authorization !== "undefined") {
-      // retrieve the authorization header and parse out the JWT using the split function
+      // Récupération de l'en-tête d'autorisation et analyse du JWT à l'aide de la fonction split
       let token = req.headers.authorization.split(" ")[1];
-      // Here we validate that the JSON Web Token is valid
+      // Validation du jeton Web JSON 
       jwt.verify(token, 'my_secret_key', (err, payload) => {
       if (err) {
       res.status(401).json({ error: "Not Authorized" });
       }
-      req.user = payload; // allow to use the user id in the controller
+      req.user = payload; // Utilisation de l'identifiant de l'utilisateur dans le contrôleur
       console.log(req.user);
       return next(); }); }
   }
 
-let router = express.Router();
 
-//send list of users
 router.get('/users', userController.userList);
 router.post('/user', userController.userNew);
 router.post('/login', userController.login);
